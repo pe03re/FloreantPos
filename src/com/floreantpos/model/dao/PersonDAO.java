637a6dda381153802a7c7647fba78692963c8697
somya <somya89@gmail.com>
2015-04-27 01:31:53 +0530

    person dao changes

--- src/com/floreantpos/model/dao/PersonDAO.java
+++ src/com/floreantpos/model/dao/PersonDAO.java
@@ -1,6 +1,6 @@
 package com.floreantpos.model.dao;
 
-public class PersonDAO extends BasePackSizeDAO {
+public class PersonDAO extends BasePersonDAO {
 
 	/**
 	 * Default constructor. Can be used in place of getInstance()
