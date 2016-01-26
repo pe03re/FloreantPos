6c6deb474aa731eeeb7f0c3685587f4b3c50a8c8
somya <somya89@gmail.com>
2016-01-26 12:41:38 +0530

    ticket detail report

--- src/com/floreantpos/main/Application.java
+++ src/com/floreantpos/main/Application.java
@@ -98,7 +98,7 @@ public class Application {
 		if (developmentMode) {
 			pluginManager.addPluginsFrom(new File("C:/MyProjects/InventoryPlugin/orostock/target/classes").toURI());
 			pluginManager.addPluginsFrom(new File("C:/MyProjects/CustPlugin/orocust/target/classes").toURI());
-			pluginManager.addPluginsFrom(new File("C:/MyProjects/ReportPlugin/orderDetailReport/target/classes").toURI());
+			pluginManager.addPluginsFrom(new File("C:/MyProjects/ReportPlugin/ticketDetailReport/target/classes").toURI());
 			pluginManager.addPluginsFrom(new File("C:/MyProjects/ReportPlugin/menuItemDetailReport/target/classes").toURI());
 			pluginManager.addPluginsFrom(new File("C:/MyProjects/ReportPlugin/dayWiseReport/target/classes").toURI());
 			pluginManager.addPluginsFrom(new File("C:/MyProjects/ReportPlugin/voidDetailReport/target/classes").toURI());
