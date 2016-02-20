756a99e8b74a178017f264e82c3da852d2a9869b
somya <somya89@gmail.com>
2016-02-20 22:39:28 +0530

    added a camel case method for string

--- src/com/floreantpos/report/ReportUtil.java
+++ src/com/floreantpos/report/ReportUtil.java
@@ -118,6 +118,24 @@ public class ReportUtil {
 			IOUtils.closeQuietly(resource);
 		}
 	}
+	
+	public static String toCamelCase(final String init) {
+	    if (init==null)
+	        return null;
+
+	    final StringBuilder ret = new StringBuilder(init.length());
+
+	    for (final String word : init.split(" ")) {
+	        if (!word.isEmpty()) {
+	            ret.append(Character.toUpperCase(word.charAt(0)));
+	            ret.append(word.substring(1).toLowerCase());
+	        }
+	        if (!(ret.length()==init.length()))
+	            ret.append(" ");
+	    }
+
+	    return ret.toString();
+	}
 
 	public static void main(String[] args) {
 		try {
